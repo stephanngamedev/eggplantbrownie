bd58dadec448d354faa91c42d16850488177927a
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2015-09-20 09:45:55 -0300

    Movendo ViewControler e Main.storyboard

---
+++ eggplan-brownie/viewcontrollers/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  eggplan-brownie
+//
+//  Created by Stephann Vasconcelos on 9/19/15.
+//  Copyright © 2015 Stepo Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var happinessField: UITextField!
+    
+    @IBAction func add(){
+        if nameField == nil || happinessField == nil {
+            return
+        }
+
+        let name = nameField.text!
+        let happiness = Int( happinessField.text! )
+        
+        let meal = Meal( name: name, happiness: happiness! )
+        
+        print( meal )
+    }
+}
+
