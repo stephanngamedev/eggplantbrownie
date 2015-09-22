fb2f4221fa35b26b32db607bfe9e8ef9d79d06ea
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2015-09-22 14:07:36 -0300

    Adicionando seleção de items ao criar uma refeição

--- eggplan-brownie/viewcontrollers/ViewController.swift
+++ eggplan-brownie/viewcontrollers/ViewController.swift
@@ -12,11 +12,21 @@ protocol AddAMealDelegate {
     func add( meal: Meal )
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet var nameField: UITextField!
     @IBOutlet var happinessField: UITextField!
     
     var delegate: AddAMealDelegate?
+    var items = [
+        Item(name: "Eggplant Brownie", calories: 10),
+        Item(name: "Zuccini Muffin", calories: 10),
+        Item(name: "Cookie", calories: 10),
+        Item(name: "Coconut oil", calories: 500),
+        Item(name: "Chocolate frosting", calories: 1000),
+        Item(name: "Chocolate chip", calories: 1000)
+    ]
+    
+    var selectedItems = Array<Item>()
     
     @IBAction func add(){
         if nameField == nil || happinessField == nil {
@@ -31,7 +41,8 @@ class ViewController: UIViewController {
         }
         
         let meal = Meal( name: name, happiness: happiness! )
-        
+        meal.items = selectedItems
+        print( meal.items )
         if delegate == nil {
             return
         }
@@ -41,6 +52,40 @@ class ViewController: UIViewController {
         if let navigation = self.navigationController {
             navigation.popViewControllerAnimated( true )
         }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let item = items[row]
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+        cell.textLabel?.text = item.name
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        let cell = tableView.cellForRowAtIndexPath( indexPath )
+        
+        if cell == nil {
+            return nil
+        }
+        
+        if cell!.accessoryType == UITableViewCellAccessoryType.None {
+            cell!.accessoryType = UITableViewCellAccessoryType.Checkmark
+            selectedItems.append( items[indexPath.row] )
+        } else {
+            cell!.accessoryType = UITableViewCellAccessoryType.None
+            if let position = selectedItems.indexOf( items[indexPath.row]) {
+                selectedItems.removeAtIndex( position )
+            }
+        }
+        
+        cell!.selected = false
+
+        return nil
     }
 }
 
